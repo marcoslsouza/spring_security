3a0adb6946e73c2861e840711b96a35deeb5970d
Marcos L. Souza <marcoslsouzadf@gmail.com>
2020-11-29 22:58:06 -0300

    Implementando o login e o logout

--- src/main/java/com/mballem/curso/security/config/SecurityConfig.java
+++ src/main/java/com/mballem/curso/security/config/SecurityConfig.java
@@ -10,6 +10,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
-		http.authorizeRequests().antMatchers("/", "/home", "/webjars/**", "/css/**", "/image/**", "/js/**").permitAll().anyRequest().authenticated();
+		http.authorizeRequests()
+		.antMatchers("/", "/home", "/webjars/**", "/css/**", "/image/**", "/js/**")
+		.permitAll()
+		.anyRequest()
+		.authenticated()
+		.and()
+			.formLogin()
+			.loginPage("/login")
+			.defaultSuccessUrl("/", true)
+			.failureUrl("/login-error")
+			// .permitAll() Para que todo usuario tenha acesso a página de login e pagina de erro
+			.permitAll()
+		.and()
+			// Por padrão ele vai redirecionar para a página de login ao realizar o logout, mas iremos redirecionar para a pagina raiz da aplicacao 
+			.logout()
+			.logoutSuccessUrl("/");
 	}
 }
--- src/main/java/com/mballem/curso/security/web/controller/HomeController.java
+++ src/main/java/com/mballem/curso/security/web/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.mballem.curso.security.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,5 +11,24 @@ public class HomeController {
 	@GetMapping({"/", "/home"})
 	public String home() {
 		return "home";
-	}	
+	}
+	
+	// abrir pagina login
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	// abrir login invalido
+	@GetMapping("/login-error")
+	public String loginError(ModelMap model) {
+		
+		model.addAttribute("alerta", "erro");
+		model.addAttribute("titulo", "Credenciais inválidas!");
+		model.addAttribute("texto", "Login ou senha incorretos, tente novamente.");
+		model.addAttribute("subtexto", "Acesso permitido apenas para cadastros já ativados.");
+		
+		// Quando nao houver credenciais validas ele retorna para a pagina de login.
+		return "login";
+	}
 }
